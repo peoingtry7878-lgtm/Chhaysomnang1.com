0afe24a340b9bf21b28c0fe66f9287c7aff20f87
peoingtry7878-lgtm <peoingtry7878@gmail.com>
2025-10-03 16:03:39 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const username =[
+    "poeing chentry",
+    "poeing chentry",
+    "poeing chentry",
+    "poeing chentry",
+    "poeing chentry",
+    "poeing chentry",
+];
+let i;
+for( i=0; i<username.length; i++){
+    console.log(`user #${i+1}  ${username[i]}`);
+}
